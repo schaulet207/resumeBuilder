1efb0c88f82fd52ce71c12dc8911c80df5a8b66a
Shannon Chaulet <shannon.chaulet@gmail.com>
2024-05-11 02:01:42 -0400

    Updates the styling on the toggle button.

--- resumeBuilder/src/components/personal/inputs/EmailInput.jsx
+++ resumeBuilder/src/components/personal/inputs/EmailInput.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function EmailInput({ email, onEmailChange }) {
     return (
         <input
+            id="email"
             className="inputs"
             type="email"
             placeholder="Enter email"
